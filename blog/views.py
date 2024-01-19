6382da9df9f48ec3664061baa5b3f1c97ff216e6
santosandre7 <dev.training.andre@gmail.com>
2024-01-18 22:12:23 -0400

    Aula 431 - Finalizada

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def blog(request):
+
+    context = {
+            'text': 'Olá little blog',
+            'title': 'Essa é uma página de exemplo - '
+        }    
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        context,
     )
 
 
 def exemplo(request):
+
+    context = {
+            'text': 'Estamos na home'
+        }
     return render(
         request,
-        'blog/exemplo.html'
+        'blog/exemplo.html',
+        context,
     )
--- home/views.py
+++ home/views.py
@@ -2,7 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
+
+    context = {
+            'text': 'Agora estou via context'
+        }
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context,
     )
